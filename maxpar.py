45782366b1873e103a108738a53a89875b53ff2c
laghsal riwan <20233810@etud.univ-evry.fr>
2026-03-26 14:22:35 +0100

    add fonction qui get toutes les dependendances d'une tache

--- maxpar.py
+++ maxpar.py
@@ -20,3 +20,32 @@ class TaskSystem:
         self.tasks = {}
         for t in liste_taches:
             self.tasks[t.name] = t
+
+def _get_tous_les_ancetres(self, nomTache):
+    tous_les_ancetres = set()
+
+    def fouiller_parents(tache_courante):
+        parents_direct = self.precedence.get(tache_courante, [])
+
+        for parent in parents_direct:
+            if parent not in tous_les_ancetres:
+                tous_les_ancetres.add(parent)
+                fouiller_parents(parent)
+
+    fouiller_parents(nomTache)
+
+    return list(tous_les_ancetres)
+
+def getDependencies(self, nomTache):
+
+    dependances_maximales = []
+    tache_cible = self.tasks[nomTache]
+
+    ancetres = self._get_tous_les_ancetres(nomTache)
+
+    for nom_ancetre in ancetres:
+        tache_ancetre = self.tasks[nom_ancetre]
+        if interfere(tache_ancetre, tache_cible):
+            dependances_maximales.append(nom_ancetre)
+
+    return dependances_maximales
